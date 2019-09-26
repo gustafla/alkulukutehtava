250025972a5084d35911f79aceca7f915294f541
Lauri Gustafsson <me@gustafla.space>
2019-09-26 18:12:29 +0300

    Remove debug assertion and clean up. Add readme.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Prime number search
+
+This program was written as a fun little Rust excercise.
+It uses the Sieve of Eratosthenes -algorithm to find primes.
+CLI only in finnish.
+
+# Alkulukuhaku
+
+Kirjoitin tämän ohjelman huvin vuoksi.
+Toiminta perustuu Erastotheneen seulaan.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use std::collections::HashMap;
 use std::io::{self, BufRead, Write};
 use std::str::FromStr;
-use std::collections::HashMap;
 
 fn kysy<T: FromStr>(kysymys: &str) -> T {
     let mut input = String::new();
@@ -22,21 +22,20 @@ fn main() {
     let alaraja: usize = kysy("Anna alueen alaraja");
     let ylaraja: usize = kysy("Anna alueen yläraja");
 
-    // TODO could possibly use only a map
     let mut alkuluvut = vec![true; ylaraja + 1];
     let mut tekijat = HashMap::with_capacity(ylaraja / 20);
 
     for i in (2usize..).take_while(|x| x * x <= ylaraja) {
         if alkuluvut[i] {
-                for j in (0..).map(|x| i * i + x * i).take_while(|x| x <= &ylaraja) {
+            for j in (0..).map(|x| i * i + x * i).take_while(|x| x <= &ylaraja) {
                 alkuluvut[j] = false;
-                tekijat.insert(j, (i, j/i));
+                tekijat.insert(j, (i, j / i));
             }
         }
     }
 
     let mut viimeinen = 0;
-    for i in alaraja..ylaraja+1 {
+    for i in alaraja..ylaraja + 1 {
         print!("{} ", i);
         if i < 2 {
             println!("ei ole kelvollinen alkuluku.");
@@ -45,10 +44,6 @@ fn main() {
             viimeinen = i;
         } else {
             let (n, m) = tekijat.get(&i).unwrap();
-            if cfg!(debug_assertions) {
-                assert_eq!(n * m, i);
-                print!("Assertion passed");
-            }
             println!("ei ole alkuluku, koska {} * {} = {}", n, m, i);
         }
     }
